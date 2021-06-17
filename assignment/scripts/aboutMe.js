601331bba5296e646c456a4e73e33c216ae68a23
Benjamin Henke <benjaminmhenke@gmail.com>
2021-06-17 14:32:11 -0500

    Completed question 13 & 14.

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -58,8 +58,11 @@ else {
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-if (luckyNumber === 2, adventurous) {
+if (luckyNumber == 2 && adventurous) {
   console.log("Roll the dice!");
+}
+else {
+  console.log("No dice!");
 }
 
 // 15 - Write a conditional that console logs "I can have more pets!"
@@ -67,8 +70,14 @@ if (luckyNumber === 2, adventurous) {
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-if (pets <= allowedPets) {
-  console.log('I have enough pets');
+if (pets < allowedPets) {
+  console.log("I have enough pets");
+}
+else if (pets == allowedPets) {
+  console.log("I have enough pets");
+}
+else if (pets > allowedPets) {
+  console.log("Oh no, I have too many pets!");
 }
 
 // STRETCH GOALS:
